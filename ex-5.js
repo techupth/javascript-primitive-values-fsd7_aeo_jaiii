c5c821e961fd75d5750dcbab7cbb172ee97c50ff
Thammaruthai <thammaruthai@gmail.com>
2024-09-30 14:15:14 +0700

    feat:answer exercise of primitive value

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPriceBeforeDiscount = 27000;
+let discountPrice= totalPriceBeforeDiscount*(25/100);
+let totalPriceAfterDiscount = totalPriceBeforeDiscount-discountPrice;
+
+console.log(totalPriceAfterDiscount);
